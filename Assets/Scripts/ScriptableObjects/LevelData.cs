266f1c7d7d8b11aac790b2997d9855e5360fbba2
alokright <alokright@gmail.com>
2023-12-01 04:17:45 +0530

    levelData menu fixes

--- Assets/Scripts/ScriptableObjects/LevelData.cs
+++ Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System;
 
-[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObject/LevelData", order = 1)]
 public class LevelData : ScriptableObject, IComparable<LevelData>
 {
     public int Index;
