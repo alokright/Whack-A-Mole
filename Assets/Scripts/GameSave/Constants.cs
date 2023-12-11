1d17b09a6ba7ee8aba0bedcd755c938dc3bbde37
Alok Kumar <>
2023-12-12 01:42:32 +0530

    ISavedGameState1.cs class renamed to Constants

---
+++ Assets/Scripts/GameSave/Constants.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Constants
+{
+    public const string LEVEL_ID_KEY = "p_l_id";
+    public const string LEVEL_SCORE_KEY = "p_l_sc";
+    public const string LEVEL_LIVES_CONSUMED_KEY = "p_l_lcon";
+    public const string LEVEL_AD_WATCHED_KEY = "p_l_ad";
+}
--- Assets/Scripts/GameSave/ISaveGameState1.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-public interface ISaveGameState
-{
-    Dictionary<string, object> SaveGameData(Dictionary<string, object> data);
-    void SetGameResumeData(Dictionary<string, object> data);
-}
